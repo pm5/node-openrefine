b296126307b9140d1f6b8ac898873118ef8cb447
Pomin Wu <pomin5@gmail.com>
2015-11-16 19:42:20 +0800

    refactor tests

--- test/openrefine.js
+++ test/openrefine.js
@@ -1,83 +1,101 @@
+'use strict'
 
 var expect = require('chai').expect
 var OpenRefine = require('../').OpenRefine
 
-describe('OpenRefine API', function () {
+describe('OpenRefine', () => {
+  var test_project_name = 'my_awesome_data_cleanup_project_dont_use_this_name_for_your_projects'
 
   describe('Server', () => {
-    it('can get projects metadata', done => {
-      OpenRefine()
-        .projects_metadata()
-        .then(data => {
-          if (!data.projects) {
-            return done('Incorrect data')
-          }
-          Object.keys(data.projects).forEach(function (id) {
-            if (!data.projects[id].name ||
-                !data.projects[id].created ||
-                !data.projects[id].modified) {
-              return done('incorrect data in project ' + id + '.')
-            }
-          })
-          done()
+    describe('get projects metadata', () => {
+      var projects_data
+      before(() =>
+        OpenRefine()
+          .projects_metadata()
+          .then(data => projects_data = data)
+      )
+
+      it('should have correct format', () => {
+        expect(projects_data.projects).to.be.defined
+        Object.keys(projects_data.projects).forEach(id => {
+          expect(projects_data.projects[id].name).to.be.defined
+          expect(projects_data.projects[id].created).to.be.defined
+          expect(projects_data.projects[id].modified).to.be.defined
         })
-        .catch(done)
+      })
     })
 
-    it('can delete projects', done => {
-      var server = OpenRefine()
-      server
-        .project('my_awesome_data_cleanup_project')
-        .upload('test/test.csv')
-        .then(r => server.delete(r.project_id))
-        .then(() => done())
-        .catch(done)
+    describe('delete projects', () => {
+      var id
+      before(() =>
+        OpenRefine()
+          .project(test_project_name)
+          .upload('test/test.csv')
+          .then(r => id = r.project_id)
+      )
+      after(() => OpenRefine().delete(id))
+
+      it('should delete projects', () =>
+        OpenRefine()
+          .delete(id)
+          .then(() => OpenRefine().projects_metadata())
+          .then(data => {
+            expect(data).to.not.have.property(id)
+          })
+      )
     })
   })
 
   describe('Project', () => {
-    it('can create project by uploading data', done => {
+    after(() =>
       OpenRefine()
-        .project('my_awesome_data_cleanup_project')
-        .upload('test/test.csv')
-        .then(r => {
-          if (!r.project_id) {
-            return done('no project ID returned.')
-          }
-          if (typeof r.project_id !== 'number' || r.project_id <= 0) {
-            return done('invalid project ID ' + r.project_id + '.')
-          }
-          done()
-        })
-        .catch(done)
+        .projects_metadata()
+        .then(data =>
+          Object.keys(data.projects)
+            .filter(id => data.projects[id].name === test_project_name)
+        )
+        .then(ids => ids.forEach(OpenRefine().delete))
+    )
+
+    describe('upload data', () => {
+      it('should create project by uploading data', () =>
+        OpenRefine()
+          .project(test_project_name)
+          .upload('test/test.csv')
+          .then(r => {
+            expect(r.project_id).to.be.defined
+            expect(r.project_id).to.be.a('number')
+            expect(r.project_id).to.be.gte(0)
+          })
+      )
     })
 
-    it('can apply operations to project', done => {
-      OpenRefine()
-        .project('my_awesome_data_cleanup_project')
-        .upload('test/test.csv')
-        .apply('test/op.json')
-        .then(() => done())
-        .catch(done)
+    describe('apply operations', () => {
+      it('should apply operations to project', () =>
+        OpenRefine()
+          .project(test_project_name)
+          .upload('test/test.csv')
+          .apply('test/op.json')
+      )
     })
 
-    it('can download data from project', done => {
-      OpenRefine()
-        .project('my_awesome_data_cleanup_project')
-        .upload('test/test.csv')
-        .apply('test/op.json')
-        .download('csv', 'output.csv')
-        .then(() => done())
-        .catch(done)
+    describe('download data', () => {
+      it('should download data from project', () =>
+        OpenRefine()
+          .project(test_project_name)
+          .upload('test/test.csv')
+          .apply('test/op.json')
+          .download('csv', 'output.csv')
+      )
     })
 
-    it('can delete projects', done => {
-      OpenRefine()
-        .project('my_awesome_data_cleanup_project')
-        .upload('test/test.csv')
-        .delete()
-        .then(() => done())
-        .catch(done)
+    describe('delete project', () => {
+      it('should delete projects', () =>
+        OpenRefine()
+          .project(test_project_name)
+          .upload('test/test.csv')
+          .delete()
+      )
     })
   })
 })
